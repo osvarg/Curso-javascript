96036cc9882cfb6601f314a8a3a48ad0d3920cb2
Soy Oscar <oscar.svg31@gmail.com>
2024-06-17 22:49:27 -0600

    17 de junio

---
+++ Imprimir 3 en 3.js
@@ -0,0 +1,10 @@
+// 1 - 4 - 7 - 10
+// 1 -2 -5 -8 -11
+let maximo = 10, resultado = 1, mínimo = -10;
+for(let contador = 1; contador <= maximo; contador += 3){
+    console.log(contador);
+}
+for(let contador = 1; contador >= mínimo; contador -= 3){
+    console.log(contador);
+}
+
