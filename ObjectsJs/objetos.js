3693b7493b41684a12faddbd340292b86951369f
Soy Oscar <oscar.svg31@gmail.com>
2024-06-26 06:19:57 -0600

    26 de junio

---
+++ ObjectsJs/objetos.js
@@ -0,0 +1,20 @@
+let x = 10;
+console.log(x.length)
+
+let persona = {
+    nombre: 'Juan',
+    apellido: 'Perez',
+    email: 'jperez@mail.com',
+    edad: 28,
+    nombreCompleto: function(){
+        return this.nombre + ' ' + this.apellido
+    }
+}
+
+console.log(persona.nombre)
+console.log(persona.apellido)
+console.log(persona.email)
+console.log(persona.edad)
+console.log(persona.nombreCompleto())
+console.log(persona)
+
