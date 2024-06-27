06b95f6a6ec3b3485d4484966589f1311b9e51df
cholito junior <oscar.svg31@gmail.com>
2024-06-27 17:54:30 -0600

    27 de junio laptop

--- ObjectsJs/Constructor objetos.js
+++ ObjectsJs/Constructor objetos.js
@@ -2,10 +2,17 @@ function Persona(nombre, apellido, email){
     this.nombre = nombre;
     this.apellido = apellido;
     this.email = email;
+    this.nombreCompleto = function(){
+        return.this
+    }
 }
 
 let padre = new Persona('Juan', 'Perez', 'jperez@mail.com');
-console.log(padre)
+console.log(padre);
 
 let madre = new Persona('Juana', 'Rodriguez', 'jrodriguez@email.com');
-console.log(madre)
+console.log(madre);
+
+padre.nombre = 'Carlos';
+console.log(padre);
+console.log(madre);
