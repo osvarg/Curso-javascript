4c3e150ab7d0f60e7f54e4d5e21bcbc7c2fdebc8
Soy Oscar <oscar.svg31@gmail.com>
2024-06-27 21:57:02 -0600

    27 de junio

--- ObjectsJs/Constructor objetos.js
+++ ObjectsJs/Constructor objetos.js
@@ -3,15 +3,17 @@ function Persona(nombre, apellido, email){
     this.apellido = apellido;
     this.email = email;
     this.nombreCompleto = function(){
-        return.this
+        return this.nombre + ' ' + this.apellido;
     }
 }
 
 let padre = new Persona('Juan', 'Perez', 'jperez@mail.com');
 console.log(padre);
+console.log(padre.nombreCompleto());
 
 let madre = new Persona('Juana', 'Rodriguez', 'jrodriguez@email.com');
 console.log(madre);
+console.log(madre.nombreCompleto());
 
 padre.nombre = 'Carlos';
 console.log(padre);
---
+++ ObjectsJs/Formas creación objetos.js
@@ -0,0 +1,18 @@
+// Ambos dan exactamente el mismo resultado, pero la ultima es la mejor practica:
+let miObjeto = new Object();
+let miObjeto2 = {};
+
+let miCadena1 = newString();
+let miCadena2 = 'Hola';
+
+let miNumero = new Number(1);
+let miNumero2 = 1;
+
+let miBoolean = new Boolean(false);
+let miBoolean2 = false;
+
+let miArreglo1 = new Array();
+let miArreglo2 = [];
+
+let miFuncion = new Function();
+let miFuncion2 = function(){};
